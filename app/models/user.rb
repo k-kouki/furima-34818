1256fccacdedd4504d7b6743baffdd2e22f80128
k-kouki <kouki0504@gmail.com>
2021-03-22 16:45:58 +0900

    ユーザー管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  validates :password, format: {with: /\A[a-z\d]{6,}+\z/i, message: "Include both letters and numbers"}
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"} 
+    validates :first_name_reading, format: {with: /\A[ｧ-ﾝﾞﾟァ-ヶー－）]+\z/, message: "Full-width katakana characters"}
+    validates :last_name_reading, format: {with: /\A[ｧ-ﾝﾞﾟァ-ヶー－）]+\z/, message: "Full-width katakana characters"}
+    validates :birthday
+  end
+
+  validates :email, uniqueness: true
+end
