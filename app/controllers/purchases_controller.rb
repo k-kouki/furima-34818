b0157b73e93eba7c9ec6e84a9d941dfdc2f83257
k-kouki <kouki0504@gmail.com>
2021-03-30 21:24:24 +0900

    購入機能修正１

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -37,7 +37,7 @@ class PurchasesController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id
+    if current_user.id == @item.user_id || @item.purchase.present?
       redirect_to root_path
     end
   end
